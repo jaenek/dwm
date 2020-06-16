2c3f82eb203ab8fd3f9f344c70b769213fec75f7
jaenek <jaenek@protonmail.com>
2020-06-17 01:53:21 +0200

    config

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int borderpx    = 1;        /* border pixel of windows */
 static const unsigned int snap        = 32;       /* snap pixel */
-static const int swallowfloating    = 0;          /* 1 means swallow floating windows by default */
+static const int swallowfloating      = 0;        /* 1 means swallow floating windows by default */
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const char buttonbar[]         = "✖";
-static const char *fonts[]            = { "monospace:size=10" };
+static const char *fonts[]            = { "monospace:bold:size=10" };
 #include "/home/jaenek/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
